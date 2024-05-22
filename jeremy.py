0631ed7af4404bd3e98c82fd87f66246e41f488d
Rayosuki <jeremy.piton2006@gmail.com>
2024-05-22 15:52:17 +0200

    deplacements

--- jeremy.py
+++ jeremy.py
@@ -1,12 +1,29 @@
 import pyxel
+
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="Nuit du Code")
+        pyxel.load("4.pyxres")
         self.x = 0
+        self.y = 0
         pyxel.run(self.update, self.draw)
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        self.x, self.y = Mouvements(self.x, self.y).bouger()
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.bltm(0, 0, 0, 0, 0, 128, 128)
+        pyxel.blt(self.x, self.y, 0, 0, 16, 8, 8)
+
+class Mouvements():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def bouger(self):
+        if pyxel.btn(pyxel.KEY_Q):
+            self.x -= 1
+        if pyxel.btn(pyxel.KEY_D):
+            self.x += 1
+        return self.x, self.y
+    
+
 App()
