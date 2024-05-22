039188572865558c2a835b8f0d73af44169a130d
Benoit Nathael <benoitnathael@gmail.com>
2024-05-22 19:59:23 +0200

    readme

--- nathael.py
+++ nathael.py
@@ -1,12 +1,14 @@
 import pyxel
+import pyxel
+import math
 class App:
     def __init__(self):
-        pyxel.init(128, 128, title="Nuit du Code")
+        pyxel.init(120, 120, title="Nuit du Code")
         self.x = 0
+        self.y = 50 
+        self.t = 0
+        self.angle = 0 
+        self.saut= False
+        pyxel.load("4.pyxres")
+        pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
-    def fond(self):
-        pass
-        
-
-
-App()
