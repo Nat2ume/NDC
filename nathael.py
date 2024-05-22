6e7daa345db808c2a323bc1fd32f067665dd9287
Benoit Nathael <benoitnathael@gmail.com>
2024-05-22 20:01:21 +0200

    Update nathael.py

--- nathael.py
+++ nathael.py
@@ -3,12 +3,6 @@ import pyxel
 import math
 class App:
     def __init__(self):
-        pyxel.init(120, 120, title="Nuit du Code")
-        self.x = 0
-        self.y = 50 
-        self.t = 0
-        self.angle = 0 
-        self.saut= False
         pyxel.load("4.pyxres")
         pyxel.playm(0, loop=True)
-        pyxel.run(self.update, self.draw)
+
